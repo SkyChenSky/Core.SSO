9c00c2cb6aae4ad75301347ad030fee9c6a74850
陈珙 <skychensky@outlook.com>
2018-09-30 17:14:23 +0800

    data protect

---
+++ SSO.Web.Core/Helper/DataProtectHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSO.Web.Core.Helper
+{
+    public static class AESHelper
+    {
+        public static byte[] Encrypt(byte[] bytesToBeEncrypted, string key)
+        {
+            byte[] encryptedBytes;
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(key);
+
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            var saltBytes = new byte[] { 13, 34, 27, 67, 189, 255, 104, 219, 122 };
+
+            using (var ms = new MemoryStream())
+            {
+                using (var aes = new RijndaelManaged())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var keyByt = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    aes.Key = keyByt.GetBytes(32);
+                    aes.IV = keyByt.GetBytes(16);
+
+                    aes.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, aes.CreateEncryptor(),
+                        CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public static byte[] Decrypt(byte[] bytesToBeDecrypted, string key)
+        {
+            byte[] decryptedBytes;
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(key);
+
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            var saltBytes = new byte[9] { 13, 34, 27, 67, 189, 255, 104, 219, 122 };
+
+            using (var ms = new MemoryStream())
+            {
+                using (var aes = new RijndaelManaged())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var keyByte = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    aes.Key = keyByte.GetBytes(32);
+                    aes.IV = keyByte.GetBytes(16);
+
+                    aes.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+    }
+}
--- SSO.Web.Core/Startup.cs
+++ SSO.Web.Core/Startup.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,18 +26,21 @@ namespace SSO.Web.Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
                {
                    options.Cookie.Name = "Token";
-                   //options.Cookie.Domain = ".chengong.com";
+#if !DEBUG
+                   options.Cookie.Domain = ".sso.com";
+#endif
                    options.Cookie.HttpOnly = true;
                    options.Cookie.Expiration = DateTime.Now.AddMinutes(30).TimeOfDay;
                    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
                    options.LoginPath = "/Account/Login";
                    options.LogoutPath = "/Account/Logout";
                    options.SlidingExpiration = true;
+                   //options.DataProtectionProvider = DataProtectionProvider.Create(new DirectoryInfo(@"D:\sso\qwe"));
+                   options.TicketDataFormat = new TicketDataFormat(new AesDataProtector());
                });
         }
 
@@ -53,5 +62,24 @@ namespace SSO.Web.Core
                     "{controller=Account}/{action=Login}/{id?}");
             });
         }
+
+        private class AesDataProtector : IDataProtector
+        {
+            public IDataProtector CreateProtector(string purpose)
+            {
+                return this;
+            }
+
+            public byte[] Protect(byte[] plaintext)
+            {
+                return AESHelper.Encrypt(plaintext, "!@#13487");
+            }
+
+            public byte[] Unprotect(byte[] protectedData)
+            {
+                return AESHelper.Decrypt(protectedData, "!@#13487");
+            }
+        }
+
     }
 }
